08537337bbd9116d4aaf32d88bbccb61c8646cf4
Ethan Ligon <ligon@ligon.org>
2018-02-22 10:10:52 -0800

    Add central elements of starter-kit.

---
+++ starter-kit/init.el
@@ -0,0 +1,56 @@
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the LigonLab Starter Kit, derived from
+;; http://eschulte.github.io/emacs24-starter-kit/
+;;
+;; This is the first thing to get loaded.
+;;
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(server-start)
+
+;; load Org-mode from source when the ORG_HOME environment variable is set
+(when (getenv "ORG_HOME")
+  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
+    (when (file-directory-p org-lisp-dir)
+      (add-to-list 'load-path org-lisp-dir)
+      (require 'org))))
+
+;; 24.1 needs this for compatibility?
+;(require 'org-compat)
+
+;;load the starter kit from the `after-init-hook' so all packages are loaded
+(add-hook 'after-init-hook
+ `(lambda ()
+    ;; remember this directory
+    (setq starter-kit-dir
+          ,(file-name-directory (or load-file-name (buffer-file-name))))
+    ;; only load org-mode later if we didn't load it just now
+    ,(unless (and (getenv "ORG_HOME")
+                  (file-directory-p (expand-file-name "lisp"
+                                                      (getenv "ORG_HOME"))))
+       '(require 'org))
+    ;; load up the starter kit
+    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
+
+;; Install (if necessary) & initialize el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+
+;;; init.el ends here
